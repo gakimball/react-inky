ed5abfb046ee0ca228c9773ccd60d8869d8c65bd
Geoff Kimball <geoff@geoffkimball.com>
2016-09-22 19:10:28 -0500

    Add inky() function to wrap final output in Foundation for Emails boilerplate

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,4 +1,22 @@
-const chai = require('chai');
-const chaiEnzyme = require('chai-enzyme');
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import inky from '../';
 
 chai.use(chaiEnzyme);
+
+describe('inky()', () => {
+  const input = '<div></div>';
+  let output;
+
+  before(() => {
+    output = inky(input);
+  });
+
+  it('returns a string', () => {
+    expect(output).to.be.a('string');
+  });
+
+  it('contains input HTML', () => {
+    expect(output).to.contain(input);
+  })
+});
---
+++ src/index.js
@@ -0,0 +1,44 @@
+/**
+ * Wrap an HTML string in the required boilerplate for Foundation for Emails.
+ * @prop {String} [elem=''] HTML to insert.
+ * @returns {String} Full HTML email.
+ *
+ * @example
+ * import inky, { Container } from 'react-inky';
+ * import { renderToString } from 'react-dom/server';
+ *
+ * const email = <Container />;
+ * inky(renderToString(<Container />));
+ */
+export default function inky(elem = '') {
+  return `
+    <!-- Emails use the XHTML Strict doctype -->
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+    <!-- The character set should be utf-8 -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width"/>
+    <!-- Link to the email's CSS, which will be inlined into the email -->
+    <link rel="stylesheet" href="assets/css/foundation-emails.css">
+    <style>
+      <!-- Your CSS to inline should be added here -->
+    </style>
+    </head>
+
+    <body>
+    <!-- Wrapper for the body of the email -->
+    <table class="body" data-made-with-foundation>
+      <tr>
+        <!-- The class, align, and <center> tag center the container -->
+        <td class="float-center" align="center" valign="top">
+          <center>
+            ${elem}
+          </center>
+        </td>
+      </tr>
+    </table>
+    </body>
+    </html>
+  `;
+}
