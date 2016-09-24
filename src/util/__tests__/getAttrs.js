f26fccd5e29d3029fcded3c30ccbda8f88f810d2
Geoff Kimball <geoff@geoffkimball.com>
2016-09-24 12:16:31 -0500

    Add getAttrs() function to correctly pass props from the component to its root element

---
+++ src/util/__tests__/getAttrs.js
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import getAttrs from '../getAttrs';
+
+describe('getAttrs()', () => {
+  let output;
+
+  before(() => {
+    const props = {
+      large: 6,
+      small: 12,
+      style: { background: 'white' },
+      className: 'header'
+    }
+    output = getAttrs(props, 'table', 'row');
+  });
+
+  it('returns an object', () => {
+    expect(output).to.be.an('object');
+  });
+
+  it('filters out custom props', () => {
+    expect(output).to.have.all.keys(['style', 'className']);
+  });
+
+  it('adds class names', () => {
+    expect(output.className).to.equal('row header');
+  });
+});
---
+++ src/util/getAttrs.js
@@ -0,0 +1,35 @@
+import { filterPropsFor } from 'react-attrs-filter';
+import classnames from 'classnames';
+
+/**
+ * Compile attributes for a component's root element, based on its tag and default class name.
+ * @param {Object} props - Component props.
+ * @param {String} tag - Component root element.
+ * @param {String} className - Component root class name.
+ * @returns {Object} Filtered props.
+ *
+ * @example
+ * const props = {
+ *   large: 6,
+ *   small: 12,
+ *   style: { background: 'white' },
+ *   className: 'header'
+ * }
+ *
+ * // Only returns "style" and "className", because the other two are custom props
+ * // The class "header" is added to the base "row"
+ * const attrs = getAttrs(props, 'table', 'row'); // => { style: ..., className: 'row header' }
+ * <table {...attrs}></table>
+ */
+export default function getAttrs(props, tag, className = '') {
+  // Filter out non-HTML attributes
+  const output = filterPropsFor(props, tag);
+
+  // Append class names in props to base classes
+  output.className = classnames(className, props.className);
+
+  // Style isn't included in `filterPropsFor` and must be copied manually
+  if (props.style) output.style = props.style
+
+  return output;
+}
