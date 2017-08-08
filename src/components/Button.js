706a38a3c3a3ed3bbc331b8698eade54059bf797
Carlos Rodrigues <carlos@jp7.com.br>
2017-08-08 19:15:11 -0300

    Migrate React.PropTypes to prop-types
    Result from running: jscodeshift -t react-codemod/transforms/React-PropTypes-to-prop-types.js react-inky/
    See: https://facebook.github.io/react/blog/2017/04/07/react-v15.5.0.html#migrating-from-react.proptypes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import getAttrs from '../util/getAttrs';
 
 export default function Button(props) {
--- src/components/Column.js
+++ src/components/Column.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import containsRow from '../util/containsRow';
 import getAttrs from '../util/getAttrs';
 import getColumnClasses from '../util/getColumnClasses';
