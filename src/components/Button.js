0dd9ad3c6bc022a055159ffd6f467bea9a794a07
Carlos Rodrigues <carlos@jp7.com.br>
2017-08-09 18:10:48 -0300

    Move PropTypes import to keep React as first import

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 import getAttrs from '../util/getAttrs';
 
 export default function Button(props) {
